0b29487b033657a01aadb73c33db5e592a02e0ef
Narcís <noguebn@gmail.com>
2017-01-15 10:58:51 +0100

    Created a canvas

---
+++ main.js
@@ -0,0 +1,14 @@
+var cols, rows;
+var w = 40;
+
+
+function setup() {
+	createCanvas(400,400);
+	cols = floor(width/w);
+	rows = floor(height/w);
+	console.log(cols,rows);
+}
+
+function draw() {
+	background(51);
+}
